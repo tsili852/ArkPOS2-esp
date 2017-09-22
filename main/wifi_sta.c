a90b4166395a359776c0890a5731ad4d0162e14d
tsili852 <nick.tsilivis@gmail.com>
2017-09-22 13:28:40 +0200

    Implement OTA wake up v4

--- main/wifi_sta.c
+++ main/wifi_sta.c
@@ -89,7 +89,8 @@ esp_err_t wifi_sta_init(wifi_sta_init_struct_t *param)
     // Start WIFI according to the current configuration.
     ESP_LOGD(TAG, "wifi_sta_init: esp_wifi_start");
     esp_err_t start_result = esp_wifi_start();
-    printf("%d", start_result);
+    ESP_ERROR_CHECK(ESP_ERROR_CHECK);
+    // printf("%d", start_result);
     if (ESP_OK != start_result) {
         ESP_LOGE(TAG, "wifi_sta_init: esp_wifi_set_config failed: %d", start_result);
         return start_result;
