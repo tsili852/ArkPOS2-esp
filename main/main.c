cedd55f1111427db8c291aa88d24f65a827ed665
tsili852 <nick.tsilivis@gmail.com>
2017-09-25 16:30:08 +0200

    test 25/09 BLE

--- main/main.c
+++ main/main.c
@@ -444,7 +444,7 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
             {
 
                 char test[256];
-                test = &param->write.value;
+                test = param->write.&value;
 
                 printf("incoming:%s\n", test);
                 // uint16_t descr_value = param->write.value[1]<<8 | param->write.value[0];
