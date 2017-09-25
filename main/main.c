a46925e1ab154cfc9f44c9b61eca59a884c3c6ec
tsili852 <nick.tsilivis@gmail.com>
2017-09-25 15:58:45 +0200

    test 25/09 BLE

--- main/main.c
+++ main/main.c
@@ -444,6 +444,8 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
             if (param->write.len > 3)
             {
                 printf("incoming:%s", param->write.value);
+                char* substr;
+                strncpy(substr, param->write.value, 3);
                 // char *incoming_message = param->write.value;
                 // save_table_number(incoming_message);    
             }
