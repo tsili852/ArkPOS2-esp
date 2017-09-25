e43e53279c99b34b27b073a080f74ac5c4da55cb
tsili852 <nick.tsilivis@gmail.com>
2017-09-25 16:03:17 +0200

    test 25/09 BLE

--- main/main.c
+++ main/main.c
@@ -447,7 +447,7 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
                 uint16_t descr_value = param->write.value[1]<<8 | param->write.value[0];
                 // char* substr;
                 // strcpy(substr, param->write.value);
-                printf("%s\n", descr_value);
+                printf("%s\n", param->write.value);
                 // strncpy(substr, substr, 3);
                 // char *incoming_message = param->write.value;
                 // save_table_number(incoming_message);    
