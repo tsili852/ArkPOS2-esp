ee8788e5ce7c4eea2f5e98f15a284e357cc35ae2
tsili852 <nick.tsilivis@gmail.com>
2017-09-26 10:46:30 +0200

    26/09 BLE->NVS

--- main/main.c
+++ main/main.c
@@ -1218,7 +1218,7 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
                 }
                 else
                 {
-                    esp_err_t err_nvs_write = nvs_set_i32(my_handle, "table_number", converted);
+                    esp_err_t err_nvs_write = nvs_set_i32(my_handle, "table_number", table_number);
                     if (err_nvs_write != ESP_OK)
                     {
                         switch (err_nvs_write) {
