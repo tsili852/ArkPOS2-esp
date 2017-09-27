c43deddf2982866f9212caeb770afdedc2613f1c
tsili852 <nick.tsilivis@gmail.com>
2017-09-27 13:42:15 +0200

    27/09 BLE augmentation

--- main/main.c
+++ main/main.c
@@ -41,7 +41,7 @@
 #define LED_GPIO_T3 26
 #define LED_GPIO_T4 21
 
-#define TOUCH_1 0 
+#define TOUCH_1 0
 #define TOUCH_2 3
 #define TOUCH_3 5
 #define TOUCH_4 7
@@ -62,6 +62,7 @@ static void check_for_updates();
 static void touch_pad_events();
 static void calibrate_touch_pads();
 static void read_thresh_from_nvs();
+static void read_mode_from_nvs();
 static void read_table_number_from_nvs();
 static void evaluate_touched_pads(int touch_counter);
 static void init_ota();
@@ -221,12 +222,19 @@ int32_t touch4_thresh;
 nvs_handle my_handle;
 
 int table_number = 0;
+/*
+------ Mode -----------
+0 => not initialized
+1 => normal
+2 => configuration mode
+*/
+int mode = 1;
 
 void app_main()
 {
     esp_err_t err_nvs = nvs_flash_init();
     if (err_nvs == ESP_ERR_NVS_NO_FREE_PAGES) {
-        // NVS partition was truncated and needs to be erased        
+        // NVS partition was truncated and needs to be erased
         ESP_ERROR_CHECK(nvs_flash_erase());
         // Retry nvs_flash_init
         err_nvs = nvs_flash_init();
@@ -243,15 +251,17 @@ void app_main()
     else
     {
         ESP_LOGI(TAG, "NVS Opened");
-        read_thresh_from_nvs();        
+        read_thresh_from_nvs();
+        read_mode_from_nvs();
         read_table_number_from_nvs();
     }
 
-    if (table_number == 0)
+    if (table_number == 0 || mode == 2)
     {
-        ESP_LOGI(TAG, "Waiting for table number");
+        ESP_LOGW(TAG, "Entering Configuration State");
+        ESP_LOGW(TAG, "Please connect to the module");
         ble_process();
-    }    
+    }
 
     struct timeval now;
     gettimeofday(&now, NULL);
@@ -268,48 +278,48 @@ void app_main()
         }
         case ESP_SLEEP_WAKEUP_TOUCHPAD: {
             printf("Wake up from touch pad on T%d\n", esp_sleep_get_touchpad_wakeup_status());
-            
+
             touch_pad_init();
-            
+
             // Read value from touch_pad 1
-            
+
             uint16_t touch_1_val = 0;
             uint16_t touch_1_threshold = 0;
-            
+
             touch_pad_read(0, &touch_1_val);
 
             touch_pad_get_thresh(TOUCH_1, &touch_1_threshold);
-        
+
             printf("T1:%d with Trh: %d\n", touch_1_val, touch_1_threshold);
 
             // Read value from touch_pad 2
 
-            uint16_t touch_2_val = 0;        
+            uint16_t touch_2_val = 0;
             uint16_t touch_2_threshold = 0;
             touch_pad_read(TOUCH_2, &touch_2_val);
 
             touch_pad_get_thresh(TOUCH_2, &touch_2_threshold);
-        
+
             printf("T2:%d with Trh: %d\n", touch_2_val, touch_2_threshold);
 
             // Read value from touch_pad 3
 
-            uint16_t touch_3_val = 0;        
+            uint16_t touch_3_val = 0;
             uint16_t touch_3_threshold = 0;
             touch_pad_read(TOUCH_3, &touch_3_val);
 
             touch_pad_get_thresh(TOUCH_3, &touch_3_threshold);
-        
+
             printf("T3:%d with Trh: %d\n", touch_3_val, touch_3_threshold);
 
             // Read value from touch_pad 4
 
-            uint16_t touch_4_val = 0;        
+            uint16_t touch_4_val = 0;
             uint16_t touch_4_threshold = 0;
             touch_pad_read(TOUCH_4, &touch_4_val);
 
             touch_pad_get_thresh(TOUCH_4, &touch_4_threshold);
-        
+
             printf("T4:%d with Trh: %d\n", touch_4_val, touch_4_threshold);
 
             uint16_t touch_1_thresh_activate = touch_1_threshold - 2000;
@@ -384,7 +394,7 @@ void app_main()
     // const long long wakeup_time_sec = 10; // 10 seconds ONLY FOR TEST
     printf("Wake up timer set to %lld seconds.\n", wakeup_time_sec);
     esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
-    
+
     touch_pad_init();
     touch_pad_filter_start(10);
     touch_pad_set_meas_time(0x1000, 0xffff);
@@ -394,15 +404,15 @@ void app_main()
 
     printf("Touch pad wake up configured\n");
     esp_sleep_enable_touchpad_wakeup();
-    
+
     init_led();
-    
+
     gettimeofday(&sleep_enter_time, NULL);
 
     printf("Closing the NVS handle for the thresholds\n");
-    nvs_close(my_handle);    
+    nvs_close(my_handle);
 
-    if (table_number > 0)
+    if (table_number > 0 || mode == 1)
     {
         ESP_LOGI(TAG, "Table number: %d", table_number);
         ESP_LOGI(TAG, "Entering Deep Sleep");
@@ -413,7 +423,7 @@ void app_main()
 static void ble_process()
 {
     esp_err_t ret;
-    
+
         // Initialize NVS.
         ret = nvs_flash_init();
         if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
@@ -421,14 +431,14 @@ static void ble_process()
             ret = nvs_flash_init();
         }
         ESP_ERROR_CHECK( ret );
-    
+
         esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
         ret = esp_bt_controller_init(&bt_cfg);
         if (ret) {
             ESP_LOGE(GATTS_TAG, "%s initialize controller failed\n", __func__);
             return;
         }
-    
+
         ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM);
         if (ret) {
             ESP_LOGE(GATTS_TAG, "%s enable controller failed\n", __func__);
@@ -444,7 +454,7 @@ static void ble_process()
             ESP_LOGE(GATTS_TAG, "%s enable bluetooth failed\n", __func__);
             return;
         }
-    
+
         ret = esp_ble_gatts_register_callback(gatts_event_handler);
         if (ret){
             ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
@@ -469,6 +479,35 @@ static void ble_process()
         if (local_mtu_ret){
             ESP_LOGE(GATTS_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
         }
+}
+
+static void read_mode_from_nvs()
+{
+    esp_err_t err_tn_nvs = nvs_get_i32(my_handle,"mode", &mode);
+    switch (err_tn_nvs) {
+      case ESP_OK:
+        if (mode == 1) {
+          ESP_LOGI(TAG,"Mode => Normal");
+        }
+        else
+        {
+          ESP_LOGI(TAG,"Mode => Configuration");
+        }
+      case ESP_ERR_NVS_NOT_FOUND:
+            ESP_LOGW(TAG, "Mode not yet specified");
+            break;
+      case ESP_ERR_NVS_INVALID_HANDLE:
+          ESP_LOGW(TAG,"Invalid handle!");
+          break;
+      case ESP_ERR_NVS_INVALID_NAME:
+          ESP_LOGW(TAG,"Invalid name!");
+          break;
+      case ESP_ERR_NVS_INVALID_LENGTH:
+          ESP_LOGW(TAG,"Invalid length!");
+          break;
+      default :
+          ESP_LOGE(TAG,"Error (%d) reading mode!", err_tn_nvs);
+    }
 }
 
 static void read_table_number_from_nvs()
@@ -538,7 +577,7 @@ static void read_thresh_from_nvs()
         default :
             ESP_LOGE(TAG,"Error (%d) reading!", err_nvs);
     }
-    
+
     err_nvs = nvs_get_i32(my_handle, "t3_thresh", &touch3_thresh);
     switch (err_nvs) {
         case ESP_OK:
@@ -594,6 +633,72 @@ static void evaluate_touched_pads(int touch_counter) {
     else if (touch_counter == 2)
     {
         ESP_LOGI(TAG, "Two buttons touched");
+
+        ESP_LOGW(TAG, "Restarting in Configuration mode");
+
+        for (size_t i = 0; i < 10; i++) {
+          gpio_pad_select_gpio(LED_GPIO_T1);
+          gpio_set_direction(LED_GPIO_T1, GPIO_MODE_OUTPUT);
+          gpio_set_level(LED_GPIO_T1, 1);
+
+          gpio_pad_select_gpio(LED_GPIO_T2);
+          gpio_set_direction(LED_GPIO_T2, GPIO_MODE_OUTPUT);
+          gpio_set_level(LED_GPIO_T2, 1);
+
+          vTaskDelay(500 / portTICK_PERIOD_MS);
+
+          gpio_pad_select_gpio(LED_GPIO_T1);
+          gpio_set_direction(LED_GPIO_T1, GPIO_MODE_OUTPUT);
+          gpio_set_level(LED_GPIO_T1, 0);
+
+          gpio_pad_select_gpio(LED_GPIO_T2);
+          gpio_set_direction(LED_GPIO_T2, GPIO_MODE_OUTPUT);
+          gpio_set_level(LED_GPIO_T2, 0);
+
+        }
+
+        mode = 2;
+
+        esp_err_t err_nvs_write = nvs_set_i32(my_handle, "mode", mode);
+        if (err_nvs_write != ESP_OK)
+        {
+            switch (err_nvs_write) {
+                case ESP_ERR_NVS_INVALID_HANDLE:
+                    ESP_LOGE(TAG,"Invalid handle!");
+                    break;
+                case ESP_ERR_NVS_READ_ONLY:
+                    ESP_LOGE(TAG,"Read only!");
+                    break;
+                case ESP_ERR_NVS_INVALID_NAME:
+                    ESP_LOGE(TAG,"Invalid name!");
+                    break;
+                case ESP_ERR_NVS_NOT_ENOUGH_SPACE:
+                    ESP_LOGE(TAG,"Not enough space!");
+                    break;
+                case ESP_ERR_NVS_INVALID_LENGTH:
+                    ESP_LOGE(TAG,"Invalid length!");
+                    break;
+                case ESP_ERR_NVS_KEY_TOO_LONG:
+                    ESP_LOGE(TAG,"Key too long");
+                    break;
+                case ESP_ERR_NVS_REMOVE_FAILED:
+                    ESP_LOGE(TAG, "Flash write failed");
+                    break;
+                default :
+                    ESP_LOGE(TAG,"Error (%d) writing mode!", err_nvs_write);
+            }
+        }
+        else
+        {
+            ESP_LOGI(TAG,"Committing updates in NVS for mode ...");
+            esp_err_t err_nvs_write = nvs_commit(my_handle);
+            printf((err_nvs_write != ESP_OK) ? "Failed to commit NVS for mode!\n" : "Updated mode in NVS\n");
+
+            ESP_LOGI(TAG, "Closing the NVS handle for the mode");
+            nvs_close(my_handle);
+            esp_restart();
+        }
+
     }
     else
     {
@@ -607,7 +712,7 @@ static void check_for_updates() {
     // Configure the application event handler.
     // The handler is centrally implemented in this module.
     // From here, we delegate the event handling to the responsible modules.
-    
+
     esp_event_loop_init(&app_event_handler, NULL);
 
     init_wifi();
@@ -645,8 +750,8 @@ static void touch_pad_events() {
     uint16_t touch_4_threshold = 0;
 
     touch_pad_init();
-    
-    touch_pad_read(TOUCH_1, &touch_1_val);    
+
+    touch_pad_read(TOUCH_1, &touch_1_val);
     touch_pad_read(TOUCH_2, &touch_2_val);
     touch_pad_read(TOUCH_3, &touch_3_val);
     touch_pad_read(TOUCH_4, &touch_4_val);
@@ -740,7 +845,7 @@ static void calibrate_touch_pads() {
                     case ESP_ERR_NVS_KEY_TOO_LONG:
                         ESP_LOGE(TAG,"Key too long");
                         break;
-                    case ESP_ERR_NVS_REMOVE_FAILED: 
+                    case ESP_ERR_NVS_REMOVE_FAILED:
                         ESP_LOGE(TAG, "Flash write failed");
                         break;
                     default :
@@ -811,7 +916,7 @@ static void calibrate_touch_pads() {
                     case ESP_ERR_NVS_KEY_TOO_LONG:
                         ESP_LOGE(TAG,"Key too long");
                         break;
-                    case ESP_ERR_NVS_REMOVE_FAILED: 
+                    case ESP_ERR_NVS_REMOVE_FAILED:
                         ESP_LOGE(TAG, "Flash write failed");
                         break;
                     default :
@@ -881,7 +986,7 @@ static void calibrate_touch_pads() {
                     case ESP_ERR_NVS_KEY_TOO_LONG:
                         ESP_LOGE(TAG,"Key too long");
                         break;
-                    case ESP_ERR_NVS_REMOVE_FAILED: 
+                    case ESP_ERR_NVS_REMOVE_FAILED:
                         ESP_LOGE(TAG, "Flash write failed");
                         break;
                     default :
@@ -951,7 +1056,7 @@ static void calibrate_touch_pads() {
                     case ESP_ERR_NVS_KEY_TOO_LONG:
                         ESP_LOGE(TAG,"Key too long");
                         break;
-                    case ESP_ERR_NVS_REMOVE_FAILED: 
+                    case ESP_ERR_NVS_REMOVE_FAILED:
                         ESP_LOGE(TAG, "Flash write failed");
                         break;
                     default :
@@ -974,17 +1079,17 @@ static void init_wifi()
 {
     ESP_LOGI(TAG, "Initialize WIFI network connection.");
     ESP_LOGI(TAG, "SSID: %s, Password: %s", WIFI_NETWORK_SSID, WIFI_NETWORK_PASSWORD);
-    
+
     wifi_params.network_ssid = WIFI_NETWORK_SSID;
     wifi_params.network_password = WIFI_NETWORK_PASSWORD;
-    
+
     wifi_sta_init(&wifi_params);
 }
 
 static void init_ota()
 {
     ESP_LOGI(TAG, "Initialising OTA firmware updating.");
-    
+
     ota_config.current_software_version = SOFTWARE_VERSION;
     ota_config.server_host_name = OTA_SERVER_HOST_NAME;
     ota_config.server_port = "8883";
@@ -994,9 +1099,9 @@ static void init_ota()
     ota_config.peer_public_key_pem = peer_public_key_pem;
     ota_config.polling_interval_s = OTA_POLLING_INTERVAL_S;
     ota_config.auto_reboot = OTA_AUTO_REBOOT;
-    
+
     iap_https_init(&ota_config);
-    
+
     // Immediately check if there's a new firmware image available.
     iap_https_check_now();
 }
@@ -1005,18 +1110,18 @@ static esp_err_t app_event_handler(void *ctx, system_event_t *event)
 {
     esp_err_t result = ESP_OK;
     int handled = 0;
-    
+
     ESP_LOGI(TAG, "app_event_handler: event: %d", event->event_id);
 
     // Let the wifi_sta module handle all WIFI STA events.
-    
+
     result = wifi_sta_handle_event(ctx, event, &handled);
     if (ESP_OK != result || handled) {
         return result;
     }
-    
+
     // TODO - handle other events
-    
+
     ESP_LOGW(TAG, "app_event_handler: unhandled event: %d", event->event_id);
     return ESP_OK;
 }
@@ -1221,7 +1326,7 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
                     printf("Not a number!!\n");
                 }
                 else
-                { 
+                {
                     esp_err_t err_nvs = nvs_open("storage", NVS_READWRITE, &my_handle);
                     if (err_nvs != ESP_OK)
                     {
@@ -1229,48 +1334,81 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
                     }
                     else
                     {
-                        ESP_LOGI(TAG, "NVS Opened for table number");                                
+                        ESP_LOGI(TAG, "NVS Opened for table number");
                     }
-                    esp_err_t err_nvs_write = nvs_set_i32(my_handle, "table_number", table_number);
-                    if (err_nvs_write != ESP_OK)
-                    {
-                        switch (err_nvs_write) {
-                            case ESP_ERR_NVS_INVALID_HANDLE:
-                                ESP_LOGE(TAG,"Invalid handle!");
-                                break;
-                            case ESP_ERR_NVS_READ_ONLY:
-                                ESP_LOGE(TAG,"Read only!");
-                                break;
-                            case ESP_ERR_NVS_INVALID_NAME:
-                                ESP_LOGE(TAG,"Invalid name!");
-                                break;
-                            case ESP_ERR_NVS_NOT_ENOUGH_SPACE:
-                                ESP_LOGE(TAG,"Not enough space!");
-                                break;
-                            case ESP_ERR_NVS_INVALID_LENGTH:
-                                ESP_LOGE(TAG,"Invalid length!");
-                                break;
-                            case ESP_ERR_NVS_KEY_TOO_LONG:
-                                ESP_LOGE(TAG,"Key too long");
-                                break;
-                            case ESP_ERR_NVS_REMOVE_FAILED: 
-                                ESP_LOGE(TAG, "Flash write failed");
-                                break;
-                            default :
-                                ESP_LOGE(TAG,"Error (%d) writing!", err_nvs_write);
-                        }
-                    }
-                    else
-                    {
-                        ESP_LOGI(TAG,"Committing updates in NVS for table number ...");
-                        esp_err_t err_nvs_write = nvs_commit(my_handle);
-                        printf((err_nvs_write != ESP_OK) ? "Failed to commit NVS for table number!\n" : "Updated table number in NVS\n");
 
-                        ESP_LOGI(TAG, "Closing the NVS handle for the thresholds");
-                        nvs_close(my_handle);
-                        esp_restart();
+                    esp_err_t err_nvs_write = nvs_set_i32(my_handle, "table_number", table_number);
+
+                    switch (err_nvs_write) {
+                        case ESP_OK:
+                            ESP_LOGI(TAG,"Table number written");
+                            break;
+                        case ESP_ERR_NVS_INVALID_HANDLE:
+                            ESP_LOGE(TAG,"Invalid handle!");
+                            break;
+                        case ESP_ERR_NVS_READ_ONLY:
+                            ESP_LOGE(TAG,"Read only!");
+                            break;
+                        case ESP_ERR_NVS_INVALID_NAME:
+                            ESP_LOGE(TAG,"Invalid name!");
+                            break;
+                        case ESP_ERR_NVS_NOT_ENOUGH_SPACE:
+                            ESP_LOGE(TAG,"Not enough space!");
+                            break;
+                        case ESP_ERR_NVS_INVALID_LENGTH:
+                            ESP_LOGE(TAG,"Invalid length!");
+                            break;
+                        case ESP_ERR_NVS_KEY_TOO_LONG:
+                            ESP_LOGE(TAG,"Key too long");
+                            break;
+                        case ESP_ERR_NVS_REMOVE_FAILED:
+                            ESP_LOGE(TAG, "Flash write failed");
+                            break;
+                        default :
+                            ESP_LOGE(TAG,"Error (%d) writing!", err_nvs_write);
                     }
-                    
+
+                    ESP_LOGW(TAG,"Entering normal mode");
+                    mode = 1;
+
+                    err_nvs_write = nvs_set_i32(my_handle, "mode", mode);
+                    switch (err_nvs_write) {
+                        case ESP_OK:
+                            ESP_LOGI(TAG,"Mode written");
+                            break;
+                        case ESP_ERR_NVS_INVALID_HANDLE:
+                            ESP_LOGE(TAG,"Invalid handle!");
+                            break;
+                        case ESP_ERR_NVS_READ_ONLY:
+                            ESP_LOGE(TAG,"Read only!");
+                            break;
+                        case ESP_ERR_NVS_INVALID_NAME:
+                            ESP_LOGE(TAG,"Invalid name!");
+                            break;
+                        case ESP_ERR_NVS_NOT_ENOUGH_SPACE:
+                            ESP_LOGE(TAG,"Not enough space!");
+                            break;
+                        case ESP_ERR_NVS_INVALID_LENGTH:
+                            ESP_LOGE(TAG,"Invalid length!");
+                            break;
+                        case ESP_ERR_NVS_KEY_TOO_LONG:
+                            ESP_LOGE(TAG,"Key too long");
+                            break;
+                        case ESP_ERR_NVS_REMOVE_FAILED:
+                            ESP_LOGE(TAG, "Flash write failed");
+                            break;
+                        default :
+                            ESP_LOGE(TAG,"Error (%d) writing mode!", err_nvs_write);
+                    }
+
+                    ESP_LOGI(TAG,"Committing updates in NVS for table number ...");
+                    esp_err_t err_nvs_write = nvs_commit(my_handle);
+                    printf((err_nvs_write != ESP_OK) ? "Failed to commit NVS for table number!\n" : "Updated table number in NVS\n");
+
+                    ESP_LOGI(TAG, "Closing the NVS handle for the thresholds");
+                    nvs_close(my_handle);
+                    esp_restart();
+
                 }
             }
 
